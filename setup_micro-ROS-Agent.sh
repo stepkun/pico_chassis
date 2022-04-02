57970de2d5e43c939d38b04ab8514e3b929ee188
stepkun <stephan.kunz@kabelbw.de>
2022-04-02 21:20:37 +0200

    install msgs

--- setup_micro-ROS-Agent.sh
+++ setup_micro-ROS-Agent.sh
@@ -5,11 +5,15 @@ echo "Installing micro-ROS-Agent"
 
 # micro-ROS-Agent:
 cd ..
+if [ -d micro_ros_msgs ]
+then
+    echo "The micro_ros_msgs are already installed"
+else
+    git clone https://github.com/micro-ROS/micro_ros_msgs.git
+fi
 if [ -d micro-ROS-Agent ]
 then
     echo "The micro-ROS-Agent is already installed"
 else
-    git clone  https://github.com/micro-ROS/micro-ROS-Agent.git
-    cd micro-ROS-Agent
-    git submodule update --init
+    git clone https://github.com/micro-ROS/micro-ROS-Agent.git
 fi
